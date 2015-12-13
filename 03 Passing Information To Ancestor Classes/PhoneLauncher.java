69bc2e03fcb501ad28064f7c2a58faeaec866437
HS <none@email.none>
2015-12-13 17:18:05 +0000

    .

---
+++ 03 Passing Information To Ancestor Classes/PhoneLauncher.java
@@ -0,0 +1,36 @@
+public class PhoneLauncher {
+
+	public static void main(String[] args) {
+
+		PhoneLauncher launcher = new PhoneLauncher();
+		
+		launcher.launch();
+		
+	}
+
+	public void launch() {
+		
+		SmartPhone iPhone = new SmartPhone("Apple");
+		
+		iPhone.call("245-1111");
+		iPhone.call("223-1134");
+		iPhone.call("005-1234");
+		iPhone.call("345-4431");
+		iPhone.call("451-3221");
+		iPhone.call("999");
+		
+		iPhone.ringAlarm("Alarm");
+		
+		iPhone.playGame("Game!");
+		
+		iPhone.printLastNumbers();
+		
+		iPhone.browseWeb("Web time!");
+		
+		iPhone.findPosition();
+		
+		System.out.println(iPhone.getBrand());
+		
+	}
+
+}
